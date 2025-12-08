a546b4febd5f04d1e6b9dc22900d14f187b13c88
XavierL7 <xavierjeje11@gmail.com>
2025-12-08 15:52:38 -0300

    commit1

---
+++ blog/apps/posts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .models import Post
+
+from django.views.generic import ListView, DetailView
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = "posts/posts.html"
+    context_object_name = "posts"
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/posts_individuales.html"
+    context_object_name = "posts"
+    pk_url_kwarg = "id"
+    queryset = Post.objects.all()
---
+++ blog/blog/configuraciones/local.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb', # Ej: 'mi_proyecto_db'
+        'USER': 'root',         # Ej: 'root'
+        'PASSWORD': 'root',     # Contraseña del usuario MySQL
+        'HOST': 'localhost',             # O la IP de tu servidor MySQL
+        'PORT': '3306',                  # Puerto por defecto de MySQL
+    }
+}
