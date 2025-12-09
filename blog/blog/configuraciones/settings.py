5e255bf4adebb2b6aacf3f64e61fcc62e68cc362
XavierL7 <xavierjeje11@gmail.com>
2025-12-09 14:38:59 -0300

    correccion de errores

--- blog/blog/configuraciones/settings.py
+++ blog/blog/configuraciones/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.slite3',            # Puerto por defecto de MySQL
+        'NAME': BASE_DIR / 'db.sqlite3',            # Puerto por defecto de MySQL
     }
 }
 
