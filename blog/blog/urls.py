db9deb2d434bde17044c78f66094a1bd5a463be2
gonzaalzu <gonza.alzu@gmail.com>
2025-12-12 20:13:36 -0300

    Agrego correcciones

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for blog project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from .views import index
@@ -23,11 +7,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('' , index, name='index'),
-    path ('' ,  include('apps.posts.urls')),
-    path ('' ,  include('apps.contacto.urls')),
-    path ('' ,  include('apps.usuario.urls')),
-    path ('' ,  include('django.contrib.auth.urls')),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', index, name='index'),                     
+    path('posts/', include('apps.posts.urls')),        # ✅ solo una vez
+    path('contacto/', include('apps.contacto.urls')),
+    path('usuario/', include('apps.usuario.urls')),   
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
