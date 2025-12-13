95616fe68791c1b8ef2658193fc8e94d805f3842
XavierL7 <xavierjeje11@gmail.com>
2025-12-13 19:28:06 -0300

    agregue mejoras a base.html

--- blog/apps/contacto/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "contacto"  
-
-urlpatterns = [
-    path('', views.ContactoUsuario.as_view(), name='contacto'),  
-]
--- blog/apps/usuario/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.urls import reverse
-from django.contrib.auth.models import AbstractUser
-# models
-class Usuario(AbstractUser):
-    imagen = models.ImageField(null=True, blank = True, upload_to= 'usuario', default='usuario/user-default.jpg')
-    def get_absolute_url(self):
-        return reverse('index')
-# Create your models here.
